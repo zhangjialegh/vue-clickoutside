d52c15e7753643e5c8d57638a811cb980af11080
zhangjialegh <zjl1195185765@outlook.com>
2019-04-19 13:50:38 +0800

    init

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -6,7 +6,7 @@ module.exports = {
     devtool: 'source-map',
     entry: "./src/index.js",//入口文件，就是上步骤的src目录下的index.js文件，
     output: {
-        path: path.resolve(__dirname, './dist'),//输出路径，就是上步骤中新建的dist目录，
+        path: path.resolve(__dirname, '../dist'),//输出路径，就是上步骤中新建的dist目录，
         publicPath: '/dist/',
         filename: 'clickoutside.min.js',
         library: 'jialecheung',
